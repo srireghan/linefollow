dfa17a2a187d5e1476734586ebad8544452ae68f
srireghan <srireghan@CEI3232>
2025-02-21 00:31:14 +0530

    Add project files.

---
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxRaw = new System.Windows.Forms.PictureBox();
+            this.pictureBoxThreshold = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThreshold)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxRaw
+            // 
+            this.pictureBoxRaw.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxRaw.Name = "pictureBoxRaw";
+            this.pictureBoxRaw.Size = new System.Drawing.Size(374, 426);
+            this.pictureBoxRaw.TabIndex = 0;
+            this.pictureBoxRaw.TabStop = false;
+            // 
+            // pictureBoxThreshold
+            // 
+            this.pictureBoxThreshold.Location = new System.Drawing.Point(421, 12);
+            this.pictureBoxThreshold.Name = "pictureBoxThreshold";
+            this.pictureBoxThreshold.Size = new System.Drawing.Size(332, 426);
+            this.pictureBoxThreshold.TabIndex = 1;
+            this.pictureBoxThreshold.TabStop = false;
+            this.pictureBoxThreshold.Click += new System.EventHandler(this.pictureBoxThreshold_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxThreshold);
+            this.Controls.Add(this.pictureBoxRaw);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThreshold)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxRaw;
+        private System.Windows.Forms.PictureBox pictureBoxThreshold;
+    }
+}
---
+++ WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private VideoCapture _capture;
+        private Timer _frameTimer;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _capture = new VideoCapture(0);
+            _capture.ImageGrabbed += ProcessFrame; 
+            _capture.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            using (Mat frame = new Mat())
+            {
+                _capture.Retrieve(frame, 0);  
+                if (frame != null && !frame.IsEmpty)
+                {
+                    DisplayImage(frame, pictureBoxRaw);
+
+                    DisplayThresholded(frame, pictureBoxThreshold);
+                }
+            }
+        }
+
+        private void DisplayImage(Mat image, PictureBox pictureBox)
+        {
+            double ratio = Math.Min(pictureBox.Width / (double)image.Width, pictureBox.Height / (double)image.Height);
+            Size newSize = new Size((int)(image.Width * ratio), (int)(image.Height * ratio));
+            Mat resizedImage = new Mat();
+            CvInvoke.Resize(image, resizedImage, newSize);
+
+            pictureBox.Image?.Dispose();  
+            pictureBox.Image = resizedImage.ToBitmap();  
+        }
+
+        private void DisplayThresholded(Mat image, PictureBox pictureBox)
+        {
+            Mat grayImage = new Mat();
+            CvInvoke.CvtColor(image, grayImage, ColorConversion.Bgr2Gray);
+
+            Mat thresholdedImage = new Mat();
+            CvInvoke.Threshold(grayImage, thresholdedImage, 100, 255, ThresholdType.Binary);
+
+            DisplayImage(thresholdedImage, pictureBox);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (_capture != null)
+            {
+                _capture.Stop();
+                _capture.Dispose();
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void pictureBoxThreshold_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
