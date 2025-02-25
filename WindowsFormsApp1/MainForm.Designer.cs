3ba910b48149db4beaeb2c39f2e0f3bbcc23c36f
Nithish <nduraiswa@ltu.edu>
2025-02-26 03:00:44 +0530

    WindowsFormsApp1/MainForm.Designer.cs

--- WindowsFormsApp1/MainForm.Designer.cs
+++ WindowsFormsApp1/MainForm.Designer.cs
@@ -30,8 +30,16 @@
         {
             this.pictureBoxRaw = new System.Windows.Forms.PictureBox();
             this.pictureBoxThreshold = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.thresholdTrackBar = new System.Windows.Forms.TrackBar();
+            this.directionLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThreshold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxRaw
@@ -39,6 +47,7 @@
             this.pictureBoxRaw.Location = new System.Drawing.Point(12, 12);
             this.pictureBoxRaw.Name = "pictureBoxRaw";
             this.pictureBoxRaw.Size = new System.Drawing.Size(374, 426);
+            this.pictureBoxRaw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxRaw.TabIndex = 0;
             this.pictureBoxRaw.TabStop = false;
             // 
@@ -47,23 +56,95 @@
             this.pictureBoxThreshold.Location = new System.Drawing.Point(436, 12);
             this.pictureBoxThreshold.Name = "pictureBoxThreshold";
             this.pictureBoxThreshold.Size = new System.Drawing.Size(332, 426);
+            this.pictureBoxThreshold.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxThreshold.TabIndex = 1;
             this.pictureBoxThreshold.TabStop = false;
             this.pictureBoxThreshold.Click += new System.EventHandler(this.pictureBoxThreshold_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(393, 422);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(571, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(709, 422);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // thresholdTrackBar
+            // 
+            this.thresholdTrackBar.Location = new System.Drawing.Point(357, 245);
+            this.thresholdTrackBar.Maximum = 255;
+            this.thresholdTrackBar.Name = "thresholdTrackBar";
+            this.thresholdTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.thresholdTrackBar.TabIndex = 7;
+            // 
+            // directionLabel
+            // 
+            this.directionLabel.AutoSize = true;
+            this.directionLabel.Location = new System.Drawing.Point(385, 75);
+            this.directionLabel.Name = "directionLabel";
+            this.directionLabel.Size = new System.Drawing.Size(52, 16);
+            this.directionLabel.TabIndex = 8;
+            this.directionLabel.Text = "Straight";
+            this.directionLabel.Click += new System.EventHandler(this.directionLabel_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.directionLabel);
+            this.Controls.Add(this.thresholdTrackBar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBoxThreshold);
             this.Controls.Add(this.pictureBoxRaw);
             this.Name = "MainForm";
             this.Text = "MainForm";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThreshold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thresholdTrackBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +152,12 @@
 
         private System.Windows.Forms.PictureBox pictureBoxRaw;
         private System.Windows.Forms.PictureBox pictureBoxThreshold;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar thresholdTrackBar;
+        private System.Windows.Forms.Label directionLabel;
     }
 }
--- WindowsFormsApp1/MainForm.cs
+++ WindowsFormsApp1/MainForm.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace WindowsFormsApp1
 {
@@ -13,59 +16,124 @@ namespace WindowsFormsApp1
         public MainForm()
         {
             InitializeComponent();
-
             _capture = new VideoCapture(0);
-            _capture.ImageGrabbed += ProcessFrame; 
-            _capture.Start();
+            _frameTimer = new Timer { Interval = 100 };  
+            _frameTimer.Tick += FrameTimer_Tick;
+            _frameTimer.Start();
+            thresholdTrackBar.ValueChanged += ThresholdTrackBar_ValueChanged;
         }
 
-        private void ProcessFrame(object sender, EventArgs e)
+        private void FrameTimer_Tick(object sender, EventArgs e)
         {
-            using (Mat frame = new Mat())
+            Mat frame = _capture.QueryFrame();
+            CvInvoke.Flip(frame, frame, FlipType.Horizontal);
+            if (frame != null && !frame.IsEmpty)
             {
-                _capture.Retrieve(frame, 0);  
-                if (frame != null && !frame.IsEmpty)
-                {
-                    DisplayImage(frame, pictureBoxRaw);
-
-                    DisplayThresholded(frame, pictureBoxThreshold);
-                }
+                DisplayImage(frame, pictureBoxRaw);  
+                ApplyThresholdingAndSlicing(frame);  
             }
         }
 
         private void DisplayImage(Mat image, PictureBox pictureBox)
         {
-            double ratio = Math.Min(pictureBox.Width / (double)image.Width, pictureBox.Height / (double)image.Height);
-            Size newSize = new Size((int)(image.Width * ratio), (int)(image.Height * ratio));
-            Mat resizedImage = new Mat();
-            CvInvoke.Resize(image, resizedImage, newSize);
-
-            pictureBox.Image?.Dispose();  
-            pictureBox.Image = resizedImage.ToBitmap();  
+            Image<Bgr, Byte> resizedImage = image.ToImage<Bgr, Byte>().Resize(pictureBox.Width, pictureBox.Height, Inter.Linear);
+            pictureBox.Image?.Dispose();
+            pictureBox.Image = resizedImage.ToBitmap();
         }
 
-        private void DisplayThresholded(Mat image, PictureBox pictureBox)
+       
+
+        private void UpdateLabel(int sliceIndex, int count)
         {
-            Mat grayImage = new Mat();
-            CvInvoke.CvtColor(image, grayImage, ColorConversion.Bgr2Gray);
-
-            Mat thresholdedImage = new Mat();
-            CvInvoke.Threshold(grayImage, thresholdedImage, 100, 255, ThresholdType.Binary);
-
-            DisplayImage(thresholdedImage, pictureBox);
-        }
-
-        protected override void OnFormClosing(FormClosingEventArgs e)
-        {
-            if (_capture != null)
+            this.Invoke(new Action(() =>
             {
-                _capture.Stop();
-                _capture.Dispose();
+                switch (sliceIndex)
+                {
+                    case 0:
+                        label1.Text = $"Slice 1: {count}";
+                        break;
+                    case 1:
+                        label2.Text = $"Slice 2: {count}";
+                        break;
+                    case 2:
+                        label3.Text = $"Slice 3: {count}";
+                        break;
+                    case 3:
+                        label4.Text = $"Slice 4: {count}";
+                        break;
+                    case 4:
+                        label5.Text = $"Slice 5: {count}";
+                        break;
+                }
+            }));
+        }
+
+        private void ApplyThresholdingAndSlicing(Mat frame)
+        {
+            Image<Gray, Byte> grayImage = frame.ToImage<Bgr, Byte>().Convert<Gray, Byte>();
+            Image<Gray, Byte> thresholdImage = grayImage.ThresholdBinary(new Gray(thresholdTrackBar.Value), new Gray(255));
+            pictureBoxThreshold.Image = thresholdImage.ToBitmap();
+
+            int sliceWidth = thresholdImage.Width / 5;
+            int[] whiteCounts = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Rectangle sliceRect = new Rectangle(i * sliceWidth, 0, sliceWidth, thresholdImage.Height);
+                Image<Gray, Byte> slice = thresholdImage.GetSubRect(sliceRect);
+                whiteCounts[i] = CvInvoke.CountNonZero(slice);
+                UpdateLabel(i, whiteCounts[i]);
             }
-            base.OnFormClosing(e);
+
+            DetermineDirection(whiteCounts);
+        }
+
+        private void DetermineDirection(int[] whiteCounts)
+        {
+            
+            int maxIndex = Array.IndexOf(whiteCounts, whiteCounts.Max());
+
+            string direction = "Straight";
+            switch (maxIndex)
+            {
+                case 0:
+                    direction = "Sharp Left";
+                    break;
+                case 1:
+                    direction = "Mild Left";
+                    break;
+                case 2:
+                    direction = "Straight";
+                    break;
+                case 3:
+                    direction = "Mild Right";
+                    break;
+                case 4:
+                    direction = "Sharp Right";
+                    break;
+            }
+
+            UpdateDirectionLabel(direction);
+        }
+
+        private void UpdateDirectionLabel(string direction)
+        {
+            this.Invoke(new Action(() =>
+            {
+                directionLabel.Text = $"Direction: {direction}";
+            }));
+        }
+
+
+        private void ThresholdTrackBar_ValueChanged(object sender, EventArgs e)
+        {
         }
 
         private void pictureBoxThreshold_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void directionLabel_Click(object sender, EventArgs e)
         {
 
         }
